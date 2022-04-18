08b328aa991f96e1f9ca56b0c0172bae8bada83c
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-04-18 13:57:45 +0100

    feat: add node & jest packages & chng js file name

---
+++ assets/JS/scripts.js
@@ -0,0 +1,14 @@
+// Header Scripts
+
+function difficultySelector() {
+    const difficultySettings = document.getElementById('difficulty-settings');
+    if (difficultySettings.style.display === 'none') {
+        difficultySettings.style.display = 'flex';
+    } else {
+        difficultySettings.style.display = 'none';
+    }
+};
+
+
+// Game Scripts
+
--- assets/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-function difficultySelector() {
-    const difficultySettings = document.getElementById('difficulty-settings');
-    if (difficultySettings.style.display === 'none') {
-        difficultySettings.style.display = 'flex';
-    } else {
-        difficultySettings.style.display = 'none';
-    }
-};
