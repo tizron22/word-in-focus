68ca3398b9224dcf84717db293f697bcdcbc7b27
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-04-18 17:15:53 +0100

    feat: add reset game function & tidy code

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -33,11 +33,6 @@ const difficultySettings = [
 ];
 
 let currentDifficulty = document.querySelector('input[name="game-difficulty"]:checked').value;
-let currentGuesses;
-let currentWordLength;
-let gameInput;
-
-
 /** 
 * This will add an event listener to the radio buttons and change the game accordingly.
 */
@@ -46,50 +41,74 @@ function addDifficultyListener() {
     difficultyRadios.forEach(option =>{
         option.addEventListener('click', () => {
             currentDifficulty = option.value;
-            setupGame();
+            setupGame(currentDifficulty);
         });
     });
 };
 
+let currentGuesses;
+let currentWordLength;
 /**
  * This sets up the game and changes the user input array based on difficulty.
  */
-function setupGame(){
+function setupGame(difficulty){
     difficultySettings.forEach(game =>{
-        if(game.difficulty === currentDifficulty){
+        if(game.difficulty === difficulty){
             currentGuesses = game.guesses;
             currentWordLength = game.wordlength;
             createEmptyArrays(currentWordLength, currentGuesses);
+            resetGame();
         };
     });
 };
 
+let guessInput;
 /**
  * Creates empty arrays for the user inputs their guesses into.
  * @param {number} rowLength 
  * @param {number} columnLength 
  */
 function createEmptyArrays(rowLength, columnLength){
-    gameInput = [];
+    guessInput = [];
     let baseArray = [];
     for(let i = 0; i < rowLength; i++){
         baseArray.push('');
     };
     for(let i = 0; i < columnLength; i++){
-        gameInput.push(baseArray);
+        guessInput.push(baseArray);
     };
 };
 
+const gameDisplay = document.querySelector('.game');
+
+/**
+ * This will reset the game by removing the innerHTML from the div for the tiles and 
+ * then replace it with the new amount of tiles.
+ */
+function resetGame(){
+    gameDisplay.innerHTML = '';
+    guessInput.forEach((guessRow, guessRowIndex) =>{
+        const setArea = document.createElement('div');
+        setArea.setAttribute('id', 'inputRow-' + guessRowIndex);
+        guessRow.forEach((guess, guessIndex) =>{
+            const rowArea = document.createElement('div');
+            rowArea.setAttribute('id', 'inputRow-' + guessRowIndex + '-area-' + guessIndex);
+            rowArea.classList.add('tile');
+            setArea.append(rowArea);
+        });
+        gameDisplay.append(setArea);
+    });
+};
 
 
+setupGame(currentDifficulty);
 addDifficultyListener();
 
-const gameDisplay = document.querySelector('.game');
 const inputKeyboard = document.querySelector('.input');
 
 const keyboardKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
                         'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER',
-                        'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<',];
+                        'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<'];
 
 keyboardKeys.forEach(key =>{
     const createKeyboard = document.createElement('button');
