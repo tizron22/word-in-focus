9533bbed4c2ddff8eb4d2ea1fda9d3bf2db4e0a3
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-04-20 11:56:44 +0100

    feat: seperated out game settings for cleaner code

---
+++ assets/js/gameSettings.js
@@ -0,0 +1,20 @@
+/**
+ * This is the variables for the game as an Object array.
+ */
+ const difficultySettings = [
+    {
+        "difficulty" : "easy",
+        "guesses" : 7,
+        "wordlength" : 4,
+    },
+    {
+        "difficulty" : "medium",
+        "guesses" : 6,
+        "wordlength" : 5,
+    },
+    {
+        "difficulty" : "hard",
+        "guesses" : 5,
+        "wordlength" : 6,
+    },
+];
