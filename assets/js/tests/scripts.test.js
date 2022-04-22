49c6ed05b55ecd541e21be43d5d38ef4e2e0af41
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-04-22 12:28:59 +0100

    feat: apply fix for jest & run new test

--- assets/js/tests/scripts.test.js
+++ assets/js/tests/scripts.test.js
@@ -1,7 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-const createEmptyArrays = require('../scripts');
+require('whatwg-fetch');
+let createEmptyArrays;
 
 beforeAll(() => {
     let fs = require("fs");
@@ -9,17 +10,31 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
+
+    createEmptyArrays = require('../scripts');
 });
 
 
 describe("Create Array for User Answers", () =>{
-    test("Create one array within an array with 6 blank items (Hard Difficulty)", () =>{
+    test("Create one array within an array with six blank items (Hard Difficulty)", () =>{
         expect(createEmptyArrays(6, 1)).toEqual([['', '', '', '', '', ''],]);
     });
-    test("Create two arrays within an array with 5 blank items (Medium Difficulty)", () =>{
+    test("Create two arrays within an array with five blank items (Medium Difficulty)", () =>{
         expect(createEmptyArrays(5, 2)).toEqual([['', '', '', '', ''],['', '', '', '', ''],]);
     });
-    test("Create three arrays within an array with 4 blank items (Easy Difficulty)", () =>{
+    test("Create three arrays within an array with four blank items (Easy Difficulty)", () =>{
         expect(createEmptyArrays(4, 3)).toEqual([['', '', '', ''],['', '', '', ''],['', '', '', ''],]);
+    });
+    test("Create ten arrays within an array with ten blank items (For Scalability)", () =>{
+        expect(createEmptyArrays(10, 10)).toEqual([['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', ''],
+                                                    ['', '', '', '', '', '', '', '', '', '']]);
     });
 });
