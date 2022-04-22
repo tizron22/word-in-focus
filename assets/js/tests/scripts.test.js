4316ddbffca2ebb7d0376beeb25eaf2237ccc1ef
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-04-22 07:15:26 +0100

    feat: add difficulty level to the tests

--- assets/js/tests/scripts.test.js
+++ assets/js/tests/scripts.test.js
@@ -13,13 +13,13 @@ beforeAll(() => {
 
 
 describe("Create Array for User Answers", () =>{
-    test("Create one arrays within an array with 6 blank items", () =>{
+    test("Create one array within an array with 6 blank items (Hard Difficulty)", () =>{
         expect(createEmptyArrays(6, 1)).toEqual([['', '', '', '', '', ''],]);
     });
-    test("Create two arrays within an array with 5 blank items", () =>{
+    test("Create two arrays within an array with 5 blank items (Medium Difficulty)", () =>{
         expect(createEmptyArrays(5, 2)).toEqual([['', '', '', '', ''],['', '', '', '', ''],]);
     });
-    test("Create three arrays within an array with 4 blank items", () =>{
+    test("Create three arrays within an array with 4 blank items (Easy Difficulty)", () =>{
         expect(createEmptyArrays(4, 3)).toEqual([['', '', '', ''],['', '', '', ''],['', '', '', ''],]);
     });
 });
